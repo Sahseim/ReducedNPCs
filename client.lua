4545de7b62d41ee712adca6cec5a3106c23b5f67
Sahseim <matthieuscalabre125@orange.fr>
2020-03-11 20:53:20 +0100

    Customization

--- client.lua
+++ client.lua
@@ -1,10 +1,12 @@
 -- Density values from 0.0 to 1.0.
 DensityMultiplier = 0.5
+DensityMultiplierPed = 0.01
+DensityMultiplierVehicle = 0.15
 Citizen.CreateThread(function()
 	while true do
 	    Citizen.Wait(0)
-	    SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
-	    SetPedDensityMultiplierThisFrame(DensityMultiplier)
+	    SetVehicleDensityMultiplierThisFrame(DensityMultiplierVehicle)
+	    SetPedDensityMultiplierThisFrame(DensityMultiplierPed)
 	    SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
 	    SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
 	    SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
